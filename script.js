91d7de26cf08f321df134c5ce4700d6d5efee105
abir364 <abirh364@gmail.com>
2023-01-14 02:46:34 +0600

    Slider functionality implemented all required functionality has been met

--- script.js
+++ script.js
@@ -24,38 +24,60 @@ function stateChange() {
 }
 
 function gridReset(){
+    const hovered = document.querySelectorAll('.white-box');
     hovered.forEach((box)=>{
         box.style.backgroundColor = '#fff';
     });
 }
 
-const container = document.querySelector('#container');
-
-for (let i = 0; i < 12; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row')
-    container.appendChild(row);
-    for (let j = 0; j < 12; j++) {
-        const content = document.createElement('div');
-        content.classList.add('white-box');
-        row.appendChild(content);
+function generateGrid(size){
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row')
+        container.appendChild(row);
+        for (let j = 0; j < size; j++) {
+            const content = document.createElement('div');
+            content.classList.add('white-box');
+            row.appendChild(content);
+        }
     }
+
+    const black = document.getElementById('black');
+    black.addEventListener('click', toggleBlack);
+
+    const random = document.getElementById('random');
+    random.addEventListener('click', toggleRandom);
+
+    const hovered = document.querySelectorAll('.white-box');
+
+
+    hovered.forEach((box)=>{
+    box.addEventListener('mouseover', stateChange);
+    reset.addEventListener('click', gridReset);
+});
+
 }
 
+function removeGrid(){
+    con.innerHTML = '';
+    generateGrid(slider.value);
+}
 
-
+const container = document.querySelector('#container');
+const con = document.querySelector('#container');
 const reset = document.getElementById('reset');
-reset.addEventListener('click', gridReset);
-
-const black = document.getElementById('black');
-black.addEventListener('click', toggleBlack);
-
-const random = document.getElementById('random');
-random.addEventListener('click', toggleRandom);
-
-const hovered = document.querySelectorAll('.white-box');
 
 
-hovered.forEach((box)=>{
-    box.addEventListener('mouseover', stateChange);
+const slider = document.getElementById('grid-size');
+const sliderValue = document.getElementById('value');
+sliderValue.innerHTML = `${slider.value} x ${slider.value}`;
+generateGrid(slider.value);
+
+slider.addEventListener('input', () => {
+    sliderValue.innerHTML = `${slider.value} x ${slider.value}`;
+    removeGrid();
+    generateGrid(slider.value);
 });
+
+
+
