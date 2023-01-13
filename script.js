086334c267aed7b761b82935c2e5fdbbfdff7146
abir364 <abirh364@gmail.com>
2023-01-13 07:29:51 +0600

    Added functionality to buttons

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-function stateChange() {
-    this.classList.add('hovered');
+let b = 1;
+
+function toggleBlack(){
+    b = 1;
 }
 
+function toggleRandom(){
+    b = 0;
+}
+
+function stateChange() {
+    if(b === 1){
+        this.classList.add('hovered');
+    }
+    else{
+        let r = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+        let g = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+        let b = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+        this.style.backgroundColor = `rgb(${r},${g},${b})`;
+    }
+}
+
+function gridReset(){
+    hovered.forEach((box)=>{
+        box.classList.remove('hovered');
+    });
+}
 
 const container = document.querySelector('#container');
 
@@ -20,3 +43,12 @@ const hovered = document.querySelectorAll('.white-box');
 hovered.forEach((box)=>{
     box.addEventListener('mouseover', stateChange);
 });
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click', gridReset);
+
+const black = document.getElementById('black');
+black.addEventListener('click', toggleBlack);
+
+const random = document.getElementById('random');
+black.addEventListener('click', toggleRandom);
